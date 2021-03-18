840d8ab0f290f9e5a78c689bfc9f8b6f196e2b55
Malo Jaboulet <malo@cojabou.com>
2021-03-18 21:27:19 +0100

    Initial Commit

---
+++ BewerbungsApp/BewerbungsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BewerbungsAppApp.swift
+//  BewerbungsApp
+//
+//  Created by Malo Jaboulet on 18.03.21.
+//
+
+import SwiftUI
+
+@main
+struct BewerbungsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
